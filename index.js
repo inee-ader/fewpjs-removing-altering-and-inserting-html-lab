4091f225616867df74e37b89051e00f54058f934
xoliio <xoliio@gmail.com>
2020-11-16 15:10:55 -0800

    badly written lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let removeMain = document.querySelector("#main");
+main.remove(removeMain);
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Inee is the champion";
+
